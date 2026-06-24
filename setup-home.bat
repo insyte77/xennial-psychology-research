c1a4401c565d324f7e490d9f3371c8251dc1e0f3
insyte77 <insyte77@gmail.com>
2026-06-24 17:04:11 +0500

    Добавить скрипт клонирования для домашнего компьютера

---
+++ setup-home.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Клонирование проекта xennial-psychology-research...
+git clone https://github.com/insyte77/xennial-psychology-research.git
+echo.
+echo Готово! Папка создана: xennial-psychology-research
+pause
